dd5b3ece92f56bcacf176a5a1ad4ff2c4cbc3095
Shishir707 <mirshr8@gmail.com>
2025-05-16 00:18:47 +0600

    updated Menu

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ else {
 }
 
 greetingElement.innerHTML=`<h2> ${greetingText} </h2>
-                          <p>Updated Home Page</P>
+                          <p>Added Catering Menu</P>
                           <h4>Your app is up to date</h4>`;
 setTimeout(() => {
   greetingElement.innerHTML = '';
